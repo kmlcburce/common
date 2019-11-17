01a81c455283e92998d125b4472ecd3b40df1a81
kennCK <kennettecanales@gmail.com>
2019-11-17 19:59:51 +0800

    update

--- notification/src/http/NotificationController.php
+++ notification/src/http/NotificationController.php
@@ -22,13 +22,17 @@ class NotificationController extends APIController
           $result[$i]['account'] = $this->retrieveAccountDetails($result[$i]['from']);
           $result[$i]['created_at_human'] = Carbon::createFromFormat('Y-m-d H:i:s', $result[$i]['created_at'])->copy()->tz('Asia/Manila')->format('F j, Y');
           if($result[$i]['payload'] == 'guarantor'){
-            $result[$i]['display'] = 'You have been assigned as guarantor by '.$result[$i]['account']['username'];
+            $result[$i]['title'] = 'Guarantor Notification';
+            $result[$i]['description'] = 'You have been assigned as guarantor by '.$result[$i]['account']['username'];
           }else if($result[$i]['payload'] == 'comaker'){
-            $result[$i]['display'] = 'You have been assigned as comaker by '.$result[$i]['account']['username'];
+            $result[$i]['title'] = 'Comaker Notification';
+            $result[$i]['description'] = 'You have been assigned as comaker by '.$result[$i]['account']['username'];
           }else if($result[$i]['payload'] == 'mail'){
-            $result[$i]['display'] = 'An email has been sent to your email address';
+            $result[$i]['title'] = 'Mail Notification';
+            $result[$i]['description'] = 'An email has been sent to your email address';
           }else if($result[$i]['payload'] == 'invest'){
-            $result[$i]['display'] = 'You have received a new investment from'.$result[$i]['account']['username'];
+            $result[$i]['title'] = 'Investment Notification';
+            $result[$i]['description'] = 'You have received a new investment from'.$result[$i]['account']['username'];
           }
           $i++;
         }
