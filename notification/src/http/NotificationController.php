b5205891d757616bb1226a8c3ae6acd9267d8d75
kennCK <kennettecanales@gmail.com>
2019-11-17 20:32:48 +0800

    update

--- notification/src/http/NotificationController.php
+++ notification/src/http/NotificationController.php
@@ -15,10 +15,17 @@ class NotificationController extends APIController
     public function retrieve(Request $request){
       $data = $request->all();
       $result = Notification::where('to', '=', $data['account_id'])->get();
-      $size = sizeof($result);
+      $size = 0;
+      $flag = false;
       if(sizeof($result) > 0){
         $i = 0;
         foreach ($result as $key) {
+          if($flag == false && $result[$i]['updated_at'] == null){
+            $size++;
+          }
+          if($flag == false && $result[$i]['updated_at'] != null){
+            $flag = true;
+          }
           $result[$i]['account'] = $this->retrieveAccountDetails($result[$i]['from']);
           $result[$i]['created_at_human'] = Carbon::createFromFormat('Y-m-d H:i:s', $result[$i]['created_at'])->copy()->tz('Asia/Manila')->format('F j, Y');
           if($result[$i]['payload'] == 'guarantor'){
@@ -41,6 +48,14 @@ class NotificationController extends APIController
         'data' => sizeof($result) > 0 ? $result : null,
         'size' => sizeof($size)
       ));
+    }
+
+    public function update(Request $request){
+      $data = $request->all();
+      $data['updated_at'] = Carbon::now();
+      $this->model = new Notification();
+      $this->updateDB($data);
+      return $this->response();
     }
 
     public function createByParams($parameter){
