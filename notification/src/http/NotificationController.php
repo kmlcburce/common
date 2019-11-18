4103c6ed57ef09f081f16327b0cb3f242dc60d3b
kennCK <kennettecanales@gmail.com>
2019-11-18 14:14:49 +0800

    update

--- notification/src/http/NotificationController.php
+++ notification/src/http/NotificationController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use Increment\Account\Models\Account;
 use Increment\Common\Notification\Models\Notification;
 use App\Http\Controllers\APIController;
+use App\Jobs\Notifications;
 class NotificationController extends APIController
 {
     function __construct(){
@@ -25,25 +26,7 @@ class NotificationController extends APIController
           }else if($flag == false && $result[$i]['updated_at'] != null){
             $flag = true;
           }
-          $result[$i]['account'] = $this->retrieveAccountDetails($result[$i]['from']);
-          $result[$i]['created_at_human'] = Carbon::createFromFormat('Y-m-d H:i:s', $result[$i]['created_at'])->copy()->tz('Asia/Manila')->format('F j, Y h:i A');
-          if($result[$i]['payload'] == 'guarantor'){
-            $result[$i]['title'] = 'Guarantor Notification';
-            $result[$i]['description'] = 'You have been assigned as guarantor by '.$result[$i]['account']['username'];
-          }else if($result[$i]['payload'] == 'comaker'){
-            $result[$i]['title'] = 'Comaker Notification';
-            $result[$i]['description'] = 'You have been assigned as comaker by '.$result[$i]['account']['username'];
-          }else if($result[$i]['payload'] == 'mail'){
-            $result[$i]['title'] = 'Mail Notification';
-            $result[$i]['description'] = 'An email has been sent to your email address';
-          }else if($result[$i]['payload'] == 'invest'){
-            $result[$i]['title'] = 'Investment Notification';
-            $result[$i]['description'] = 'You have received a new investment from'.$result[$i]['account']['username'];
-          }else{
-            //
-          }
-
-          // this is a test
+          $result[$i] = $this->manageResult($result[$i]);
           $i++;
         }
       }
@@ -51,6 +34,27 @@ class NotificationController extends APIController
         'data' => sizeof($result) > 0 ? $result : null,
         'size' => $size
       ));
+    }
+
+    public function manageResult($result){
+        $result['account'] = $this->retrieveAccountDetails($result['from']);
+        $result['created_at_human'] = Carbon::createFromFormat('Y-m-d H:i:s', $result['created_at'])->copy()->tz('Asia/Manila')->format('F j, Y h:i A');
+        if($result['payload'] == 'guarantor'){
+          $result['title'] = 'Guarantor Notification';
+          $result['description'] = 'You have been assigned as guarantor by '.$result['account']['username'];
+        }else if($result['payload'] == 'comaker'){
+          $result['title'] = 'Comaker Notification';
+          $result['description'] = 'You have been assigned as comaker by '.$result['account']['username'];
+        }else if($result['payload'] == 'mail'){
+          $result['title'] = 'Mail Notification';
+          $result['description'] = 'An email has been sent to your email address';
+        }else if($result['payload'] == 'invest'){
+          $result['title'] = 'Investment Notification';
+          $result['description'] = 'You have received a new investment from'.$result['account']['username'];
+        }else{
+          //
+        }
+        return $result;
     }
 
     public function update(Request $request){
@@ -63,7 +67,17 @@ class NotificationController extends APIController
     }
 
     public function createByParams($parameter){
-      Notification::insert($parameter);
+      $model = new Notification();
+      $model->from = $data['from'];
+      $model->to = $data['to'];
+      $model->payload = $data['payload'];
+      $model->payload_value = $data['payload_value'];
+      $model->route = $data['route'];
+      $model->created_at = $data['created_at'];
+      $model->save();
+      $result = Notification::where('id', '=', $model->id)->get();
+      $result = $this->manageResult($result[0]);
+      // Notifications::dispatch('notification', $result[0]);
       return true;
     }
 }
