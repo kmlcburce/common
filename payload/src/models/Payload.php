65b5c7148ee8fe439746cd46fdc7fa7974eb257a
kennck <kennettecanales@gmail.com>
2020-10-01 10:19:04 +0800

    udpate

--- payload/src/models/Payload.php
+++ payload/src/models/Payload.php
@@ -5,7 +5,7 @@ namespace Increment\Common\Payload\Models;
 use Illuminate\Database\Eloquent\Model;
 use App\APIModel;
 
-class Rating extends APIModel
+class Payload extends APIModel
 {
     protected $table = 'payloads';
     protected $fillable = ['account_id', 'payload', 'payload_value' ];
