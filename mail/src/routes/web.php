36503121dc463a704d1c3efb781c65674ddcbe68
kennck <kennettecanales@gmail.com>
2021-07-26 15:29:40 +0800

    update

--- mail/src/routes/web.php
+++ mail/src/routes/web.php
@@ -4,3 +4,6 @@ $controller = 'Increment\Common\Mail\Http\MailController@';
 Route::any($route.'bounces', $controller.'onBounce');
 Route::any($route.'complaints', $controller.'onComplaint');
 Route::any($route.'deliveries', $controller.'onDelivery');
+
+
+// http://7d4068667a81.ngrok.io/project123/api/public/increment/v1/aws-sns/bounces
