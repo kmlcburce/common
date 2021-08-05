0f6d299a0d8941c76768b992a95f4b83cab727ed
= <=>
2021-08-05 23:00:17 +0800

    update

--- comment/src/models/Comment.php
+++ comment/src/models/Comment.php
@@ -16,7 +16,7 @@ class Comment extends APIModel
   }
 
   public function getCreatedAtAttribute($value){
-    return Carbon::createFromFormat($this->dateTimeFormat, $value)->copy()->diffForHumans();
+    return Carbon::createFromFormat($this->dateTimeFormat, $this->formatDateTime($value))->copy()->diffForHumans();
   }
 
 }
