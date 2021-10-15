c6830504692a63317c050c76c51ce170743c3870
kennck <kennettecanales@gmail.com>
2021-10-15 14:46:03 +0800

    udpate

--- image/src/routes/web.php
+++ image/src/routes/web.php
@@ -11,48 +11,46 @@ Route::post($route.'upload_un_link', $controller."uploadUnLink");
 Route::post($route.'delete', $controller."delete");
 Route::get($route.'test', $controller."test");
 
-// $route = env('PACKAGE_ROUTE', '');
-// Route::get($route.'/storage/image/{filename}', function ($filename)
-// {
-//     $path = storage_path('/app/images/' . $filename);
+$route = env('PACKAGE_ROUTE', '');
+Route::get($route.'/storage/image/{filename}', function ($filename){
+    $path = storage_path('/app/images/' . $filename);
 
-//     if (!File::exists($path)) {
-//         abort(404);
-//     }
+    if (!File::exists($path)) {
+        abort(404);
+    }
 
-//     $file = File::get($path);
-//     $type = File::mimeType($path);
+    $file = File::get($path);
+    $type = File::mimeType($path);
 
-//     $response = Response::make($file, 200);
-//     $response->header("Content-Type", $type);
+    $response = Response::make($file, 200);
+    $response->header("Content-Type", $type);
 
-//     return $response;
-// });
+    return $response;
+});
 
-// $route = env('PACKAGE_ROUTE', '').'/files/';
-// $controller = 'Increment\Common\Image\Http\ImageController@';
-// Route::post($route.'create', $controller."create");
-// Route::post($route.'retrieve', $controller."retrieve");
-// Route::post($route.'update', $controller."update");
-// Route::post($route.'upload_file', $controller."uploadFile");
-// Route::post($route.'retrieve_file', $controller."retrieveFile");
-// Route::post($route.'delete', $controller."delete");
-// Route::get($route.'test', $controller."test");
+$route = env('PACKAGE_ROUTE', '').'/files/';
+$controller = 'Increment\Common\Image\Http\ImageController@';
+Route::post($route.'create', $controller."create");
+Route::post($route.'retrieve', $controller."retrieve");
+Route::post($route.'update', $controller."update");
+Route::post($route.'upload_file', $controller."uploadFile");
+Route::post($route.'retrieve_file', $controller."retrieveFile");
+Route::post($route.'delete', $controller."delete");
+Route::get($route.'test', $controller."test");
 
-// $route = env('PACKAGE_ROUTE', '');
-// Route::get($route.'/storage/file/{filename}', function ($filename)
-// {
-//     $path = storage_path('/app/files/' . $filename);
+$route = env('PACKAGE_ROUTE', '');
+Route::get($route.'/storage/file/{filename}', function ($filename){
+    $path = storage_path('/app/files/' . $filename);
 
-//     if (!File::exists($path)) {
-//         abort(404);
-//     }
+    if (!File::exists($path)) {
+        abort(404);
+    }
 
-//     $file = File::get($path);
-//     $type = File::mimeType($path);
+    $file = File::get($path);
+    $type = File::mimeType($path);
 
-//     $response = Response::make($file, 200);
-//     $response->header("Content-Type", $type);
+    $response = Response::make($file, 200);
+    $response->header("Content-Type", $type);
 
-//     return $response;
-// });
+    return $response;
+});
