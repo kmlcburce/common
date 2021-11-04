aa25ebb45c8850c45684c720b606bc5dcbf62666
GarridoL <lalaine.garrido@student.passerellesnumeriques.org>
2021-11-04 09:28:17 +0800

    updates

--- payload/src/http/PayloadController.php
+++ payload/src/http/PayloadController.php
@@ -341,4 +341,12 @@ class PayloadController extends APIController
       }
       return $this->response();
     }
+
+    public function retrievePayloads($payload, $payloadValue) {
+      $data = $request->all();
+      $res = Payload::where('deleted_at', '=', null)->where($payload, '=', $payloadValue)->get();
+
+      $this->response['data'] = sizeof($res) > 0 ? $res : [];
+      return $this->response();
+    }
 }
